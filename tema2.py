35930e087602b69bc17ecbc3a175b07abb31a729
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-04-07 01:47:36 +0300

    Update tema2.py

--- tema2.py
+++ tema2.py
@@ -1,4 +1,4 @@
-with open("input1.txt" , 'r')as f:
+with open("gramatica1.txt", 'r')as f:
     listaLitere = list(f.readline().strip()) #face o lista de litere
     listaExponenti = f.readline().strip().split() #fiecare exponent coincide ca index cu litera peste care e
     dicLitereExp = {}
@@ -11,28 +11,23 @@ with open("input1.txt" , 'r')as f:
             elemente = list(listaExponenti[i])
             for caracter in elemente:
                 if ord(caracter) >= 97 and ord(caracter) <= 122 and caracter not in dicLitereExp.keys():
-                    dicLitereExp[caracter] = {}
-    #valorile dictionarului sunt dictionare  care au ca cheie constrangerea si valoare numarul de care sunt legate
-    for litera in dicLitereExp:
-         constraints = f.readline().strip().split()
-         for cons in constraints:
-             if cons[0] == '!':
-                 dicLitereExp[litera]['!'] = cons[1:].split(',')
-             if cons[1] == '-':
-                 dicLitereExp[litera]['>'] = '-1'
-             elif cons[0] == '>':
-                 dicLitereExp[litera]['>'] = cons[1:]
-             if cons[0] == '<':
-                 dicLitereExp[litera]['<'] = cons[1:]
-         #daca nu se specifica nr minim de aparitii al literei pe pozitie o sa fie considerat 0
-         if '>' not in dicLitereExp[litera].keys():
-             dicLitereExp[litera]['>'] = '0'
-            
-            
+                    dicLitereExp[caracter] = {'>':'-1'} #toate aparitiile de litere trebuie sa fie >-1
 
-    print(listaLitere)
-    print(listaExponenti)
+    constraints = f.read().split("\n") #ai grija sa nu ai un \n de care nu stii
+    for cons in constraints:
+        if cons[1] == '!':
+            dicLitereExp[cons[0]]['!'] = list(cons[2:].split(','))
+        if cons[2] == '-':
+            dicLitereExp[cons[0]]['>'] = '-1'
+        elif cons[1] == '>':
+            dicLitereExp[cons[0]]['>'] = cons[3:]
+        if cons[1] == '<':
+            dicLitereExp[cons[0]]['<'] = cons[2:]
+
+with open("input1.txt" , 'r')as g:
+    listaCuvinte = g.read().split('\n')     #ai grija la \n ala
+
+print(listaLitere)
+print(listaExponenti)
 for litera in dicLitereExp:
     print(litera,"->",dicLitereExp[litera])
-
-##daca nu mi dai bounds pt o litera m-ai spart
