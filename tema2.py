7025b2b0fb7fc3f0ff233291fad6e05edce3088c
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-04-07 02:02:45 +0300

    Update tema2.py

--- tema2.py
+++ tema2.py
@@ -11,18 +11,18 @@ with open("gramatica1.txt", 'r')as f:
             elemente = list(listaExponenti[i])
             for caracter in elemente:
                 if ord(caracter) >= 97 and ord(caracter) <= 122 and caracter not in dicLitereExp.keys():
-                    dicLitereExp[caracter] = {'>':'-1'} #toate aparitiile de litere trebuie sa fie >-1
+                    dicLitereExp[caracter] = {'>':'0'} #toate aparitiile de litere trebuie sa fie >-1
 
     constraints = f.read().split("\n") #ai grija sa nu ai un \n de care nu stii
     for cons in constraints:
         if cons[1] == '!':
             dicLitereExp[cons[0]]['!'] = list(cons[2:].split(','))
-        if cons[2] == '-':
-            dicLitereExp[cons[0]]['>'] = '-1'
-        elif cons[1] == '>':
-            dicLitereExp[cons[0]]['>'] = cons[3:]
+        if cons[1] == '>':
+            dicLitereExp[cons[0]]['>'] = cons[2:]
         if cons[1] == '<':
             dicLitereExp[cons[0]]['<'] = cons[2:]
+        if cons[1] == '=':
+            dicLitereExp[cons[0]]['='] = cons[2:]      #asta va fi folosit doar cu un OR/AND valoarea este si in < sau >
 
 with open("input1.txt" , 'r')as g:
     listaCuvinte = g.read().split('\n')     #ai grija la \n ala
