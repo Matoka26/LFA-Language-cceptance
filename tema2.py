1c0d25cecb1bc4714f8765e2851085f65b44ce7a
Dogaru Mihail Danut <106425405+Matoka26@users.noreply.github.com>
2023-04-06 22:00:42 +0300

    Update tema2.py

--- tema2.py
+++ tema2.py
@@ -3,11 +3,15 @@ with open("input1.txt" , 'r')as f:
     listaExponenti = f.readline().strip().split() #fiecare exponent coincide ca index cu litera peste care e
     dicLitereExp = {}
     #creaza dictionaru cu literele care apar la exponent
+    #daca e doar o cifra o lasa asa,nu i mai face dic
     for i in range(len(listaExponenti)):
-        elemente = list(listaExponenti[i])
-        for caracter in elemente:
-            if ord(caracter) >= 97 and ord(caracter) <= 122 and caracter not in dicLitereExp.keys():
-                dicLitereExp[caracter] = {}
+        if listaExponenti[i].isdigit():
+            listaExponenti[i] = int(listaExponenti[i])
+        else:
+            elemente = list(listaExponenti[i])
+            for caracter in elemente:
+                if ord(caracter) >= 97 and ord(caracter) <= 122 and caracter not in dicLitereExp.keys():
+                    dicLitereExp[caracter] = {}
     #valorile dictionarului sunt dictionare  care au ca cheie constrangerea si valoare numarul de care sunt legate
     for litera in dicLitereExp:
          constraints = f.readline().strip().split()
